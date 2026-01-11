9f830675e82f886fa627b4d03aae317e3a37ef39
Sinyu Jung <jungsinyu@gmail.com>
2026-01-11 13:16:12 +0900

    auth共有

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -88,6 +88,6 @@ class AdminPanelProvider extends PanelProvider
             )
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])->authGuard('web');
     }
 }
