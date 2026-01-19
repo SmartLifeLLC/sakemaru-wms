ca152d72c6b5c3570164da346a2b58481e2f47ae
Sinyu Jung <jungsinyu@gmail.com>
2026-01-19 23:26:56 +0900

    Update app.blade.php

--- resources/views/handy/layouts/app.blade.php
+++ resources/views/handy/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=480, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'HANDY 入庫処理システム')</title>
 
